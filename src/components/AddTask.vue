f73f2fa59d3b3cde0027afa0771e8b9e8ee63085
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-07-30 23:11:40 +0700

    Update AddTask.vue

---
+++ src/components/AddTask.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <input v-model="task" @keyup.enter="submitTask" placeholder="Add a new task" />
+      <button @click="submitTask">Add Task</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        task: ''
+      };
+    },
+    methods: {
+      submitTask() {
+        if (this.task.trim()) {
+          this.$emit('add-task', { text: this.task, completed: false });
+          this.task = '';
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  input {
+    width: 70%;
+    padding: 10px;
+  }
+  button {
+    padding: 10px;
+  }
+  </style>
+  

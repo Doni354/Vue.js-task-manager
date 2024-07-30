54584f6032e7836bcb7099d2839de20336ada192
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-07-30 23:11:33 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Task Manager</h1>
+    <AddTask @add-task="addTask" />
+    <TaskList :tasks="tasks" @remove-task="removeTask" @toggle-task="toggleTask" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddTask from './components/AddTask.vue'
+import TaskList from './components/TaskList.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AddTask,
+    TaskList,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks.push(task);
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    toggleTask(index) {
+      this.tasks[index].completed = !this.tasks[index].completed;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

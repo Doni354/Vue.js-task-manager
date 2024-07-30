dab3d17499a0d602569ae16ba2ac68f6d38f6500
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-07-30 23:12:13 +0700

    Update TaskItem.vue

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <li :class="{ completed: task.completed }">
+      <input type="checkbox" v-model="task.completed" @change="toggleTask" />
+      <span>{{ task.text }}</span>
+      <button @click="removeTask">Remove</button>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: ['task', 'index'],
+    methods: {
+      removeTask() {
+        this.$emit('remove-task', this.index);
+      },
+      toggleTask() {
+        this.$emit('toggle-task', this.index);
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .completed span {
+    text-decoration: line-through;
+  }
+  button {
+    margin-left: 10px;
+  }
+  </style>
+  

e53d567999aea8152e9862ab09f791a9b64d1681
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-07-30 23:11:57 +0700

    Update TaskList.vue

---
+++ src/components/TaskList.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul>
+      <TaskItem
+        v-for="(task, index) in tasks"
+        :key="index"
+        :task="task"
+        :index="index"
+        @remove-task="removeTask"
+        @toggle-task="toggleTask"
+      />
+    </ul>
+  </template>
+  
+  <script>
+  import TaskItem from './TaskItem.vue'
+  
+  export default {
+    components: {
+      TaskItem
+    },
+    props: ['tasks'],
+    methods: {
+      removeTask(index) {
+        this.$emit('remove-task', index);
+      },
+      toggleTask(index) {
+        this.$emit('toggle-task', index);
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  </style>
+  

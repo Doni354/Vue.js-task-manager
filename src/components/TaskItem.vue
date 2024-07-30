f0e862207eb446509af04ffc2b005aeb228476ee
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-07-30 23:16:39 +0700

    Update TaskItem.vue

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,8 +1,10 @@
 <template>
     <li :class="{ completed: localTask.completed }">
       <input type="checkbox" v-model="localTask.completed" @change="toggleTask" />
-      <span>{{ localTask.text }}</span>
+      <span v-if="!isEditing" @dblclick="editTask">{{ localTask.text }}</span>
+      <input v-else v-model="newText" @keyup.enter="saveTask" @blur="saveTask" />
       <button @click="removeTask">Remove</button>
+      <button @click="startEditing">Edit</button>
     </li>
   </template>
   
@@ -11,7 +13,9 @@
     props: ['task', 'index'],
     data() {
       return {
-        localTask: { ...this.task } // Copy props to local data
+        localTask: { ...this.task },
+        isEditing: false,
+        newText: this.task.text,
       };
     },
     methods: {
@@ -20,17 +24,58 @@
       },
       toggleTask() {
         this.$emit('toggle-task', this.index);
-      }
+      },
+      startEditing() {
+        this.isEditing = true;
+        this.newText = this.localTask.text;
+      },
+      saveTask() {
+        if (this.newText.trim()) {
+          this.isEditing = false;
+          this.localTask.text = this.newText;
+          this.$emit('edit-task', this.index, this.newText);
+        }
+      },
     }
   }
   </script>
   
   <style scoped>
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    border-bottom: 1px solid #ddd;
+  }
+  
   .completed span {
     text-decoration: line-through;
+    color: #aaa;
   }
+  
+  input[type="text"] {
+    flex: 1;
+    margin-right: 10px;
+    padding: 5px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+  }
+  
   button {
-    margin-left: 10px;
+    margin-left: 5px;
+    padding: 5px 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #f0f0f0;
+  }
+  
+  button:active {
+    background-color: #ddd;
   }
   </style>
   
